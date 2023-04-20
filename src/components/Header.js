2ef2ef98fbe75cee1a31be743df6dbff2db2ff23
JonPaulCode <jwatson88@live.co.uk>
2023-04-20 09:37:51 +0100

    Fix user avatar overflowing on small screens

--- src/components/Header.js
+++ src/components/Header.js
@@ -40,7 +40,13 @@ export default function Header() {
   return (
     <header className="bg-[#052962] pt-6 font-bold text-white sm:px-6 xl:px-12">
       <div className="mx-auto max-w-7xl">
-        <SignIn />
+        <div className="mb-6 flex items-center justify-between px-4 sm:px-0">
+          <h1>
+            <div className="xs:text-4xl text-3xl sm:text-5xl">Northcoders</div>
+            <div className="xs:text-2xl ml-8 sm:text-3xl">News</div>
+          </h1>
+          <SignIn />
+        </div>
         <nav className="relative min-h-[3rem] border border-slate-500">
           {isLoading ? (
             <LoadingSpinner size={30} />
