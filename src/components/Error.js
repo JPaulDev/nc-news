c8f2073b82d1d571e871d64a517b05ef53f07619
JonPaulCode <jwatson88@live.co.uk>
2023-04-21 14:21:28 +0100

    Add go back link to error pages

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 export default function Error({ error }) {
   const { msg, status } = error;
 
@@ -5,7 +7,15 @@ export default function Error({ error }) {
     <section className="xs:flex-row flex h-full flex-col items-center justify-center gap-7">
       <div className="text-6xl font-bold text-sky-700">{status}</div>
       <div className="xs:block hidden h-48 w-[0.125rem] bg-sky-700" />
-      <h2 className="text-center">{msg}</h2>
+      <div className="xs:items-start flex flex-col items-center gap-3">
+        <h2 className="text-center">{msg}</h2>
+        <Link
+          className="rounded-lg bg-sky-700 px-3 py-2 font-semibold text-white shadow-xl"
+          to="/"
+        >
+          Go to homepage
+        </Link>
+      </div>
     </section>
   );
 }
